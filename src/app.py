4d99dc8fcfae1fed236f896809b8d55f72f7cbcb
sylenthmen <manuelene26@gmail.com>
2024-02-29 20:54:49 -0600

    add toml format

--- src/app.py
+++ src/app.py
@@ -1,11 +1,13 @@
 import streamlit as st
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 import openai
 
-load_dotenv()
+#load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
 
 st.title("Generador de imagenes con DallE")
 
